8cfbcfa6c4c984928c393388cba1713a7eccdfa5
miguelnleao <miguelleao.leo@gmail.com>
2025-06-19 16:35:11 +0100

    CH3 L4 Done

---
+++ functions/run_python_file.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+from google.genai import types
+
+def run_python_file(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(abs_file_path):
+        return f'Error: File "{file_path}" not found.'
+    if not abs_file_path.endswith('.py'):
+        return f'Error: "{file_path}" is not a Python file.'
+    try:
+        result = subprocess.run(["python3", file_path], capture_output=True, cwd=working_directory, timeout=30)
+        if result == None:
+            return "No output produced."
+        final = f'STDOUT: {result.stdout}\nSTDERR: {result.stderr}'
+        if result.returncode != 0:
+            final = final + f'\n Process exited with code {result.returncode}'
+        return final
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Runs python script",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file to execute",
+            ),
+        },
+    ),
+)
---
+++ functions/write_file.py
@@ -0,0 +1,37 @@
+import os
+from google.genai import types
+
+def write_file(working_directory, file_path, content):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(abs_file_path):
+        try:
+            os.makedirs(os.path.dirname(abs_file_path), exist_ok=True)
+        except Exception as e:
+            return f"Error: creating directory: {e}"
+    try:
+        with open(abs_file_path, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f'Error writing to file "{file_path}": {e}'
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes to file",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file to write to",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content that is being written",
+            ),
+        },
+    ),
+)
